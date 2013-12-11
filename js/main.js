e7e97e845d65f8a55d6cdf089036987ef8b6c9bf
Tejas Rawal <trawal90@gmail.com>
2013-12-11 10:24:49 -0600

    dlajd;kf

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-alert('hi');
+// alert('hi');
+
+var itemTemplate = $('#templates .item').html();
+
+var newItemHtml = $.render(itemTemplate, { name: 'Dewberry', price: 0.15 });
+$('#store').append(newItemHtml);
+
+
+var items = [
+  { name: 'Blowjob', price: 5 },
+  { name: 'Rimjob', price: 15 },
+  { name: 'Anal', price: 30 }
+];
+
+for(var i = 0; i < items.length; i ++) {
+}
