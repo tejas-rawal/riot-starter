f39bbda2d35dd04dece6a207db92e108c3310d80
Tejas Rawal <trawal90@gmail.com>
2013-12-11 11:44:33 -0600

    Add event handler for form

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-alert('hi');
+// alert('hi');
+
+var itemTemplate = $('#templates .item').html();
+
+$('.submit').click(function(e) {
+  e.preventDefault();
+  var item = $(".Item").val();
+  var price = $(".Price").val();
+  var newItem = $.render(itemTemplate, { name: item, price: price });
+  $('#store').append(newItem);
+});
