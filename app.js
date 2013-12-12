d9a8c0601f1e2b7d12b2b022c92e732bc8e89856
Gamble McAdam <daniel.gamble.mcadam@gmail.com>
2013-12-12 12:28:14 -0600

    stripped down app.js and added riot to vendor directory.

--- app.js
+++ app.js
@@ -20,51 +20,24 @@
   /* Listen to user events */
 
   $("#submit").click(function(e) {
-    if ($("#new-todo").val()) {
-      todo.add($("#new-todo").val());
-      $("#new-todo").val('');
-      console.log($("#new-todo").val())
+    // TODO
     }
   })
 
 
+  /* Listen to model events */
+
+  // TODO
+
 
   /* Private functions */
-  function toggle(el, flag) {
-    el.toggleClass("completed", flag);
-    $(":checkbox", el).prop("checked", flag);
-  }
+
 
   var add = function (item) {
     if (this.id) item = this;
 
     var el = $($.render(template, item)).appendTo(root),
-      input = $(".edit", el);
-      console.log(el)
 
-      function blur() {
-        el.removeClass("editing")
-      }
-
-      $(".toggle", el).click(function() {
-        todo.toggle(item.id);
-      })
-
-    // edit
-    input.blur(blur).keydown(function(e) {
-      var val = $.trim(this.value);
-      if (e.which == 13 && val) {
-        item.name = val;
-        todo.edit(item);
-      }
-
-      if (e.which == 27) blur()
-    })
-
-    $("label", el).dblclick(function() {
-      el.addClass("editing");
-      input.focus()[0].select();
-    })
 
     // remove
     $(".destroy", el).click(function() {
@@ -73,17 +46,4 @@
   }
 
 
-  /* Listen to model events */
-
-  todo.on("add", add).on("remove", function(item) {
-      $("#" + item.id).remove()
-  })
-  // }).on("toggle", function(item) {
-  //   toggle($("#" + item.id), !!item.done)
-
-  // }).on("edit", function(item) {
-  //   var el = $("#" + item.id);
-  //   el.removeClass("editing");
-  //   $("label, .edit", el).text(item.name).val(item.name);
-  // })
 })()
