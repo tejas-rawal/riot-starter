41a3d92b6ab17c650f02cfa5262f2d5033fd6e40
Gamble McAdam <daniel.gamble.mcadam@gmail.com>
2013-12-12 13:49:15 -0600

    Strip out commented solution.

--- app.js
+++ app.js
@@ -2,12 +2,7 @@
 /* The presenter */
 
 (function() { 'use strict';
-  /*
-    A Model instance exposed to global space so you can
-    use the Todo APi from the console. For example:
 
-    todo.add("My task");
-  */
   window.todo = new Todo();
 
   // HTML for a single todo item
@@ -19,18 +14,12 @@
   /* Listen to user events */
 
   // 1.
-  // $("#submit").on('click', function(e) {
-  //   // TODO
-  //   var newTodoText = $('#new-todo').val();
-  //   console.log('Adding', newTodoText);
-  //   todo.add(newTodoText);
-  // });
+
 
   // 3.
   $root.on('click', '.destroy', function (e) {
     // TODO
-    // var id = $(this).closest('.todo-item').data('id');
-    // todo.remove(id);
+
   });
 
 
@@ -39,14 +28,10 @@
 
   // 2.
   todo.on('add', function (newTodo) {
-    // var newTodoHtml = $.render(template, newTodo);
-    // $(newTodoHtml).appendTo($root);
+
   });
 
   // 4.
-  // todo.on('remove', function (newTodo) {
-  //   var dataFilter = '[data-id=' + newTodo.id + ']';
-  //   $('.todo-item', $root).filter(dataFilter).remove();
-  // });
+
 
 })();
